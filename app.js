8c55316c57ba92ed5ab43081715088077c69df54
Normando Ali Zubia Hernández <nzubia@blacklabs.mx>
2022-10-06 18:34:43 -0600

    Change place of cors

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ mongoose.connect(
 
 var app = express();
 
-app.use(cors());
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -33,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
